ed328371cb879d8f44552ba0e5acf5a8988c439a
Santosh kumar gupta <sainkee1997@gmail.com>
2024-05-10 17:24:16 +0530

    flex fixed

--- src/components/Photoad/PhotoAdd.jsx
+++ src/components/Photoad/PhotoAdd.jsx
@@ -4,7 +4,7 @@ import offer1 from "../../assets/offer-1.jpg";
 import offer2 from "../../assets/offer-2.jpg";
 export default function PhotoAdd() {
   return (
-    <div className='flex gap-5 h-[50dvh] my-10 text-center justify-between w-full '>
+    <div className='flex flex-col md:flex-row gap-5 h-[50dvh] md:my-10 mb-10 text-center justify-between w-full '>
           <StaticPhoto photo={offer1} />
           <StaticPhoto photo={offer2} />
     </div>
